b81e810f024cf24ad56ba35be1285d9bbc0720bf
Taoufik <taoufik.ajaanit@taillislabs.com>
2020-03-22 18:37:48 +0100

    feat: add active cases

--- components/Stats/index.jsx
+++ components/Stats/index.jsx
@@ -13,6 +13,7 @@ export default ({
   countries
 }) => {
   const router = useRouter();
+  const activeCases = confirmed.value - deaths.value - recovered.value
   return (
     <Wrapper as={Container}>
       {country && (
@@ -28,8 +29,8 @@ export default ({
         />
       )}
       <Flex css="margin-bottom: 4rem;" align="flex-start">
-        <Item col={4} colTablet={12} colMobile={12} gap={2} marginBottom={30}>
-          <Card color="#5E35B1">
+        <Item col={3} colTablet={12} colMobile={12} gap={2} marginBottom={30}>
+          <Card color="#ffb700">
             <h1>Confirmed</h1>
             <p>
               <NumberFormat
@@ -40,7 +41,19 @@ export default ({
             </p>
           </Card>
         </Item>
-        <Item col={4} colTablet={12} colMobile={12} gap={2} marginBottom={30}>
+        <Item col={3} colTablet={12} colMobile={12} gap={2} marginBottom={30}>
+          <Card color="#5E35B1">
+            <h1>Active Cases</h1>
+            <p>
+              <NumberFormat
+                value={activeCases}
+                displayType={"text"}
+                thousandSeparator={true}
+              />
+            </p>
+          </Card>
+        </Item>
+        <Item col={3} colTablet={12} colMobile={12} gap={2} marginBottom={30}>
           <Card color="#f44336">
             <h1>Deaths</h1>
             <p>
@@ -52,7 +65,7 @@ export default ({
             </p>
           </Card>
         </Item>
-        <Item col={4} colTablet={12} colMobile={12} gap={2}>
+        <Item col={3} colTablet={12} colMobile={12} gap={2}>
           <Card color="#4CAF50">
             <h1>Recovered</h1>
             <p>
@@ -78,7 +91,7 @@ export default ({
         data={{
           datasets: [
             {
-              data: [deaths?.value, recovered?.value, confirmed?.value],
+              data: [deaths?.value, recovered?.value, activeCases],
               backgroundColor: ["#f44336", "#4CAF50", "#5E35B1"],
               hoverBackgroundColor: ["#ff5252", "#00E676", "#7C4DFF"]
             }
